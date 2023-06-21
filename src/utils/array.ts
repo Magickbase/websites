99df531583c860a7cf2c9f243b4214bbf7af8a17
WhiteMind <WhiteMind@qq.com>
2023-06-21 18:35:01 +0800

    feat: integrating with the GitHub GraphQL API for discussions

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -3,3 +3,9 @@ import { randomInt } from './number'
 export function sample<T>(arr: T[]): T | undefined {
   return arr[randomInt(0, arr.length - 1)]
 }
+
+export const BooleanT =
+  <T>() =>
+  (a: T | '' | 0 | 0n | false | null | undefined | void): a is T => {
+    return Boolean(a)
+  }
