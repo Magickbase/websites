eb131503429705c243b95e020cf105dfe198cbcb
whitemind <whitemind@qq.com>
2023-08-08 16:18:50 +0800

    feat: implement the toc part of the post page

---
+++ packages/app/src/pages/posts/TOC/index.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps, FC, useContext } from 'react'
+import clsx from 'clsx'
+import { TOCContext } from '../../../components/TableOfContents'
+import styles from './index.module.scss'
+
+export const TOC: FC<ComponentProps<'nav'>> = props => {
+  const { tocItems } = useContext(TOCContext)
+
+  return (
+    <nav aria-label="Table of contents" {...props} className={clsx(styles.toc, props.className)}>
+      {tocItems.map(tocItem => (
+        <div key={tocItem.id} className={clsx(styles.tocItem, { [styles.active ?? '']: tocItem.isActive })}>
+          <a href={`#${tocItem.id}`}>{tocItem.title}</a>
+        </div>
+      ))}
+    </nav>
+  )
+}
