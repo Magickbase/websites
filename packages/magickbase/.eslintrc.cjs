b4fa9d6b348e834c89c6d47b1452068d3ad5ee11
WhiteMind <WhiteMind@qq.com>
2023-12-27 18:17:25 +0800

    refactor: a more appropriate lint config and tsconfig

--- packages/magickbase/.eslintrc.cjs
+++ packages/magickbase/.eslintrc.cjs
@@ -4,24 +4,19 @@ const config = {
     {
       extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
       files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: './tsconfig.json',
+        project: true,
         tsconfigRootDir: __dirname,
       },
     },
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-  },
   plugins: ['@typescript-eslint'],
   extends: ['next/core-web-vitals', 'prettier', 'plugin:@typescript-eslint/recommended'],
   rules: {
     'import/order': ['error', { groups: [['builtin', 'external', 'internal']] }],
     'import/newline-after-import': 'error',
   },
-  // This is because `next.config.mjs` is excluded from tsconfig.
-  ignorePatterns: ['next.config.mjs', 'next-i18next.config.js'],
 }
 
 module.exports = config
