d4cc3b6175a496904607c796760891049439bfbb
WhiteMind <WhiteMind@qq.com>
2024-01-26 19:22:33 +0800

    feat: add lumos project to report scripts

--- packages/scripts/src/utils/report.ts
+++ packages/scripts/src/utils/report.ts
@@ -4,7 +4,7 @@ import { ProjectItem, createDiscussion, getOrganizationProjects, getProjectItems
 import { assert } from './error'
 import { ensureFileFolderExists } from './file'
 
-const projectNames = ['CKB Explorer', 'Neuron']
+const projectNames = ['CKB Explorer', 'Neuron', 'Lumos']
 const sortedStatusValues = [
   '🆕 New',
   '📫Hold On',
@@ -23,7 +23,7 @@ const currentFilepath = join(folder, 'current.json')
 const prevFilepath = join(folder, 'prev.json')
 
 export async function updateSnapshots() {
-  const projects = await getOrganizationProjects('Magickbase')
+  const projects = [...(await getOrganizationProjects('Magickbase')), ...(await getOrganizationProjects('ckb-js'))]
   const filteredProjects = projects.filter(p => projectNames.includes(p.title))
   assert(filteredProjects.length === projectNames.length)
 
