1f942911e364aa544ae64bf7686064ea6a30f3ff
painterpuppets <painterpuppets@gmail.com>
2024-02-22 22:42:32 +0800

    chore: add scroll css for status page

--- packages/status/src/pages/_app.tsx
+++ packages/status/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 // TODO: The two files need to be merged later.
 import './globals.css'
 import '../styles/globals.scss'
+import 'overlayscrollbars/overlayscrollbars.css'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { api } from '../utils/api'
